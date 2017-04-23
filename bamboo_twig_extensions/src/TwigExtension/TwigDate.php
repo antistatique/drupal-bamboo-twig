f8d08600450378a61cd5210c2255289627fcc39a
Kevin Wenger <wenger.kev@gmail.com>
2017-04-23 20:59:05 +0200

    Fixed Bamboo Twig Extensions

---
+++ bamboo_twig_extensions/src/TwigExtension/TwigDate.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\bamboo_twig_extensions\TwigExtension;
+
+use Drupal\Core\Template\TwigEnvironment;
+
+/**
+ * Provides bridge for Text functions and filters.
+ *
+ * Expose the features of Twig_Extensions_Extension_Date.
+ */
+class TwigDate extends \Twig_Extension {
+
+  /**
+   * List of all Twig functions.
+   */
+  public function getFilters() {
+    return [
+      new \Twig_SimpleFilter('bamboo_extensions_time_diff', [$this, 'diff'], ['needs_environment' => TRUE]),
+    ];
+  }
+
+  /**
+   * Unique identifier for this Twig extension.
+   */
+  public function getName() {
+    return 'bamboo_twig_extensions.twig.date';
+  }
+
+  /**
+   * Filter for converting dates to a time ago string.
+   *
+   * @param \Drupal\Core\Template\TwigEnvironment $env
+   *   A Twig_Environment instance.
+   * @param string|DateTime $date
+   *   String or DateTime object to convert.
+   * @param string|DateTime $now
+   *   String or DateTime object to compare with.
+   *   If none given, the current time will be used.
+   *
+   * @return string
+   *   The converted time.
+   */
+  public function diff(TwigEnvironment $env, $date, $now = NULL) {
+    $extension = new \Twig_Extensions_Extension_date();
+    $filters = $extension->getFilters();
+
+    foreach ($filters as $filter) {
+      if ($filter->getName() == 'time_diff') {
+        $callable = $filter->getCallable();
+        return $callable($env, $date, $now);
+      }
+    }
+
+    return FALSE;
+  }
+
+}
---
+++ bamboo_twig_extensions/src/TwigExtension/TwigText.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\bamboo_twig_extensions\TwigExtension;
+
+use Drupal\Core\Template\TwigEnvironment;
+
+/**
+ * Provides bridge for Text functions and filters.
+ *
+ * Expose the features of Twig_Extensions_Extension_Text.
+ */
+class TwigText extends \Twig_Extension {
+
+  /**
+   * List of all Twig functions.
+   */
+  public function getFilters() {
+    return [
+      new \Twig_SimpleFilter('bamboo_extensions_truncate', [$this, 'truncate'], ['needs_environment' => TRUE]),
+    ];
+  }
+
+  /**
+   * Unique identifier for this Twig extension.
+   */
+  public function getName() {
+    return 'bamboo_twig_extensions.twig.text';
+  }
+
+  /**
+   * Truncate a string.
+   *
+   * Can't use the Twig filter callback cause the truncate function is
+   * actually declared as a global function and not method of
+   * Twig_Extensions_Extension_Text.
+   *
+   * @param \Drupal\Core\Template\TwigEnvironment $env
+   *   A Twig_Environment instance.
+   * @param string $string
+   *   The input string. Must be one character or longer.
+   * @param int $length
+   *   The string returned will contain at most length characters from beginning.
+   * @param bool $preserve
+   *   Preserving whole words or not.
+   * @param string $separator
+   *   The ellipsis to use.
+   *
+   * @return string|bool
+   *   Returns the extracted part of string; or FALSE on failure,
+   *   or an empty string.
+   */
+  public function truncate(TwigEnvironment $env, $string, $length = 30, $preserve = FALSE, $separator = '...') {
+    $extension = new \Twig_Extensions_Extension_Text();
+    $filters = $extension->getFilters();
+
+    foreach ($filters as $filter) {
+      if ($filter->getName() == 'truncate') {
+        $callable = $filter->getCallable();
+        return $callable($env, $string, $length, $preserve, $separator);
+      }
+    }
+
+    return FALSE;
+  }
+
+}
